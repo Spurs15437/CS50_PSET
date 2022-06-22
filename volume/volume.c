82e01c6db45e9d28a5f38e20793a8920feb5bdbd
rrrrlau <46474155+Spurs15437@users.noreply.github.com>
2022-06-22 22:42:44 +0800

    WEEK4 LAB4 volume

---
+++ volume/volume.c
@@ -0,0 +1,56 @@
+// Modifies the volume of an audio file
+
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+// Number of bytes in .wav header
+const int HEADER_SIZE = 44;
+
+int main(int argc, char *argv[])
+{
+    // Check command-line arguments
+    if (argc != 4)
+    {
+        printf("Usage: ./volume input.wav output.wav factor\n");
+        return 1;
+    }
+
+    // Open files and determine scaling factor
+    FILE *input = fopen(argv[1], "r");
+    if (input == NULL)
+    {
+        printf("Could not open file.\n");
+        return 1;
+    }
+
+    FILE *output = fopen(argv[2], "w");
+    if (output == NULL)
+    {
+        printf("Could not open file.\n");
+        return 1;
+    }
+
+    float factor = atof(argv[3]);
+
+    // TODO: Copy header from input file to output file
+    uint8_t header[HEADER_SIZE];
+    for (int i = 0; i < HEADER_SIZE; i++)
+    {
+        fread(&header[i], sizeof(uint8_t), 1, input);
+        fwrite(&header[i], sizeof(uint8_t), 1, output);
+    }
+
+    // TODO: Read samples from input file and write updated data to output file
+    int16_t buffer;
+// 这里必定有什么结束符号，才会说需要使用while，使用for循环在获取到整个文件字节数的前提下为什么不行
+    while (fread(&buffer, sizeof(int16_t), 1, input))
+    {
+        buffer = buffer * factor;
+        fwrite(&buffer, sizeof(int16_t), 1, output);
+    }
+
+    // Close files
+    fclose(input);
+    fclose(output);
+}
